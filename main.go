fc43dbf91ef120c934266eaab487de76b1987337
liwc02 <liwc02@mingyuanyun.com>
2022-09-15 15:23:35 +0800

    feat: add client proto

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 
 	rocket_server.RegisterAirRocketServerServer(s, server.NewAirRocketServer())
 
-	log.Printf("Starting gRPC listener on port: %v", ADDRESS)
+	log.Printf("Starting AirRocket server listener on: %v", ADDRESS)
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
